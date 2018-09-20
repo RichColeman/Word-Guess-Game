e37085988961becb0a9488fb1b4b561e684bcbfa
richcoleman <coleman.richardm@gmail.com>
2018-09-20 00:49:18 -0500

    additional changed added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,62 @@
-//phrases for the word guess game
-var phrases = ["tastykake", "wawa"];
-
-// choose a random word
+var phrases = ["tastykake", "wawa", "cheesesteak","jawn","rocky",];
 var chosenWord = phrases[Math.floor(Math.random() * phrases.length)];
-
-//initialized variables
+var chosenWordArr = chosenWord.split("");
+console.log(chosenWordArr);
 var guesses = 13;
+var correctGuesses = [];
 var letters = [];
-
-// display on the screen one underscore per letter for a specific word
-// select the element from the DOM using document
 var underscoresSpan = document.getElementById("underscores");
 var guessesCount = document.getElementById("guesses-count");
 var lettersGuessed = document.getElementById("letters-guessed");
 var verdictSpan = document.getElementById("verdict");
+var resetSpan = document.getElementById("reset");
 var underscoreWord = "";
+
 
 for (var i = 0; i < chosenWord.length; i++) {
   underscoreWord += "_";
 }
+var underscoreArr = underscoreWord.split("");
 
-// each underscore an element
-// currently reference position
+console.log(underscoreArr);
 
 underscoresSpan.innerText = underscoreWord;
 guessesCount.innerText = "Youse got " + guesses + " guesses left.";
 lettersGuessed.innerText = "Youse already guessed " + letters + " .";
 
-// take user input and check it against the specific word
-// create an event for onkeyup
 document.onkeyup = function(event) {
-  //define userGuess as an event.key
   var userGuess = event.key;
-  // push userGuess to the letters array if the user hasn't guessed it already
   if (userGuess !== letters) {
     letters.push(userGuess);
-    var randomNumber = letters[Math.floor(Math.random() * letters.length)];
-    console.log(randomNumber);
     console.log(letters);
     lettersGuessed.textContent = "Youse already guessed " + letters + ".";
-  } else if (userGuess === letters.randomNumber) {
-    lettersGuessed.innerText = "Dumb";
+  } else if (userGuess.some(letters) === true){
+    console.log("nope");
   }
+if (guesses > 0){
+  for (i = 0; i < chosenWord.length; i++) {
+    if (chosenWordArr[i] === userGuess) {
+      correctGuesses.push(i);
+      console.log(correctGuesses);
+      underscoreArr[i] = chosenWordArr[i];
+      underscoresSpan.innerText = underscoreArr.toString();
+    //   if (underscoreArr.findIndex("_") === -1){      
+    //     verdictSpan.innerText = "Holy heck, you won."
+    // } else
+  if (underscoreArr.indexOf("_") === -1) {
+    console.log ("you won");
+    verdictSpan.innerHTML = "<h3>You won! Have a hoagie</h3>";
+    resetSpan.innerHTML = "<h3>Press SHIFT to play again!</h3>";
 
-  for (var i = 0; i < chosenWord.length; i++) {
-    // check guesses number
-    if (guesses > 0) {
-      //check user guess with if else statement
-      // if guess matches a letter from the chosen word
-      if (userGuess === chosenWord[i]) {
-        var s = chosenWord.indexOf(i);
-        console.log(s);  
-        underscoreWord = underscoreWord.replace(underscoreWord, chosenWord);
-        underscoresSpan.textContent = underscoreWord;
-
-        // wrong guess decreases guess count and logs guess in html
-      } else {
-        guesses = 13 - letters.length;
-        guessesCount.textContent = "Youse got " + guesses + " guesses left.";
-        // console.log(guesses);
-      }
-      // displays loss message
-    } else {
-      verdictSpan.textContent = "Youse lost. Go get a citywide and try again.";
-      lettersGuessed.textContent = "";
-    }
+  }  
+  
+  
   }
-};
+   else {
+    guesses = 13 - letters.length;
+    guessesCount.textContent = "Youse got " + guesses + " guesses left.";
+  }
+}
+}
+}
+
